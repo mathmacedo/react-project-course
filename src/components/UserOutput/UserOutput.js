55de0204dc173ef465e2a0023cd7709e848e2944
Matheus Macedo <matheus.macedo1977@gmail.com>
2019-08-28 10:31:46 -0300

    Adicionando components UserInput e UserOutput

---
+++ src/components/UserOutput/UserOutput.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "./UserOutput.css";
+
+class UserOutput extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {};
+	}
+
+	render() {
+		return <div></div>;
+	}
+}
+
+export default UserOutput;
