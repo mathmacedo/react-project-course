a09d9c15aadd577d740ba57662dc277e86984a0d
Matheus Macedom <matheus.macedo1977@gmail.com>
2019-09-18 09:51:33 -0300

    Updates

---
+++ precache-manifest.4bbfd40e8a29159e1a910b1d57c48637.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4600255207d3e134209cd910cc9151eb",
+    "url": "/react-project-course/index.html"
+  },
+  {
+    "revision": "8b5888e753252b05f26d",
+    "url": "/react-project-course/static/css/main.4de2b594.chunk.css"
+  },
+  {
+    "revision": "bef068f98a05070ea062",
+    "url": "/react-project-course/static/js/2.032a2494.chunk.js"
+  },
+  {
+    "revision": "8b5888e753252b05f26d",
+    "url": "/react-project-course/static/js/main.e38c233a.chunk.js"
+  },
+  {
+    "revision": "c3618aeeb1968a1700db",
+    "url": "/react-project-course/static/js/runtime~main.d261be9a.js"
+  }
+]);
